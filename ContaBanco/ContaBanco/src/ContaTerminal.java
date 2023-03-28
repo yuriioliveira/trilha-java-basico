5576cb32cafc873e44c5eafc23f2f932e7b2520f
Yuri <yuricarvlahooliveira@gmail.com>
2023-03-28 17:10:10 -0300

    Update ContaTerminal

--- ContaBanco/ContaBanco/src/ContaTerminal.java
+++ ContaBanco/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Qual é o seu nome? ");
+        nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, informe a agência da sua conta. ");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor, informe o número da sua conta. ");
+        numeroConta = sc.nextInt();
+
+        System.out.println("Por favor, informe o saldo da sua conta. ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        sc.close();
     }
 }
